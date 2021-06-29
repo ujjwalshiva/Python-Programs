d7ef7a83ffb77fcdb3987231bf325092ee3b5665
Ujjwal Acharya <81429137+ujjwalshiva@users.noreply.github.com>
2021-06-29 13:58:58 +0530

    Create Check_alphabet_vowel_consonant.py

---
+++ Check_alphabet_vowel_consonant.py
@@ -0,0 +1,6 @@
+# Check if the alphabet is vowel/consonant
+letter=input("Enter any letter from a-z (small-caps): ")
+if letter == 'a' or letter =='e' or letter =='i' or letter =='o' or letter =='u':
+    print(letter, "is a Vowel")
+else:
+    print(letter, "is a Consonant")
